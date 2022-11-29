18216e7b4c2c8071bbd04a6a212506c9ce926c64
Raphaela <raphaela.prammer@gmail.com>
2022-11-29 15:08:35 +0100

    vue lab 01 with router

---
+++ LABS/VUE/01-lab-vue-setup-a/src/components/FirstComponent.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h2>Text interpolation using v-text Directive</h2>
+    <p v-text="someText"></p>
+    <hr />
+
+    <h2>Inserting a reactive Image with v-bind</h2>
+    <img class="image" v-bind:src="someImage.image" :alt="someImage.altText" />
+    <hr />
+
+    <h2>Text interpolation using v-HTML Directive</h2>
+    <div v-html="someText2"></div>
+    <hr />
+
+    <h2>Text interpolation using mustache syntax</h2>
+    <div>{{ someText3 }}</div>
+    <hr />
+
+    <h2>Hello World Button</h2>
+    <button @click="helloWorld">click me short version</button>
+    <button v-on:click="helloWorld">click 2 long version</button>
+    <hr />
+
+    <h2>Router Links</h2>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from "vue";
+
+let someText = ref(
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem iusto sit iure amet architecto corporis nemo consequatur, delectus molestiae inventore? Totam doloribus itaque rem neque tempore distinctio? Est, eum quibusdam?"
+);
+
+let someText2 = ref(`
+<div>
+    <h3>Some text 2</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium praesentium sapiente, similique facere fugit inventore laudantium animi iste explicabo obcaecati, iusto dolorem nesciunt assumenda ipsam voluptates esse reiciendis maiores. Non.</p>
+</div>
+`);
+
+let someText3 = ref("This ist some Text 3 and inserted with mustache syntax");
+
+const someImage = reactive({
+  image:
+    "https://img.elo7.com.br/product/zoom/3E0B0FF/baby-look-the-stooges.jpg",
+  altText: "stooges-pic",
+});
+
+// Hello World Button
+const helloWorld = () => {
+  alert("hello World");
+  console.log("hello World");
+};
+</script>
+
+<style scoped>
+.image {
+  width: 50%;
+}
+</style>
---
+++ LABS/VUE/01-lab-vue-setup-a/src/views/MorePics.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>This is a route to more pics</h3>
+    <img
+      src="https://www.gannett-cdn.com/presto/2020/08/07/PDTN/aa5a3b53-2c86-4881-9855-6a57cb60b83e-TDNBrd_DETROIT_06-11-2020_TDNXX_1_D002__2020_06_10_IMG_Iggy_stooges2_1_1_R62RSR87_L82530087_IMG_Iggy_stooges2_1_1_R62RSR87_1.jpg?crop=2999,1687,x0,y424&width=660&height=372&format=pjpg&auto=webp"
+      alt="1"
+    />
+
+    <img
+      src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d1/The_cover_art_for_the_2020_reissue_of_The_Stooges_1969_debut_album.png/220px-The_cover_art_for_the_2020_reissue_of_The_Stooges_1969_debut_album.png"
+      alt="2"
+    />
+  </div>
+</template>
+
+<script setup></script>
+
+<style>
+img {
+  width: 50%;
+}
+</style>
